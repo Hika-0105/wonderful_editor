153232d6e49072270bc1db995b765df660b18818
佐藤　光 <hikahu0105@gmail.com>
2021-11-22 16:27:56 +0900

    Task7-3 テストまで終了

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  context"タイトルが存在する時" do
+    let(:article){build(:article)}
+
+    it"保存できる" do
+     expect(article).to be_valid
+    end
+   end
+
+   context"タイトルがが存在しない時" do
+    let(:article){build(:article, title: nil)}
+    it"エラーが起きる" do
+     expect(article).not_to be_valid
+    end
+   end
+end
