4236baf60ba91a9d7e535863d58a0ead9298b9c6
佐藤　光 <hikahu0105@gmail.com>
2021-12-04 16:58:21 +0900

    uodateのテストを実装

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -56,13 +56,34 @@ RSpec.describe "Api::V1::Articles", type: :request do
   context "適切なパラメータが送信された時" do
   let(:params){{article: attributes_for(:article)}}
 
-   fit "レコードを作成する" do
+   it "レコードを作成する" do
     expect { subject }.to change {Article.where(user_id: current_user.id).count }.by(1)
     res = JSON.parse(response.body)
     expect(res["title"]).to eq params[:article][:title]
     expect(res["body"]).to eq params[:article][:body]
     expect(response).to have_http_status(200)
    end
+  end
+
+  context "不適切なパラメータが送信された時" do
+  let(:params){attributes_for(:article)}
+   it "エラーする" do
+   expect(subject).to raise_error(ActionController::ParameterMissing)
+   end
+  end
+ end
+
+ describe "PATCH /articles/:id" do
+  before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+  let(:current_user){create(:user)}
+  subject { patch(api_v1_article_path(article_id), params: params) }
+  let(:article_id){article.id}
+  let(:article){create(:article, user: current_user)}
+  let(:params){{article: {title:Faker::Lorem.word, created_at: 1.day.ago}}}
+
+  it "ログインユーザーの記事の修正を行う" do
+   expect { subject }.to change { article.reload.title }.from(article.title).to(params[:article][:title]) &
+                         not_change { article.reload.created_at }
   end
  end
 end
