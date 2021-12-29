215e2fe6c2fe193a8451ac3deb0269ebc37242c2
佐藤　光 <hikahu0105@gmail.com>
2021-12-29 14:57:47 +0900

    新規登録APIのテストの修正

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -20,16 +20,16 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
         subject
         res = response.header
         expect(res["access-token"]).to be_present
-        expect(res["access-token"]).to be_present
-        expect(res["access-token"]).to be_present
-        expect(res["access-token"]).to be_present
-        expect(res["access-token"]).to be_present
+        expect(res["client"]).to be_present
+        expect(res["uid"]).to be_present
+        expect(res["expiry"]).to be_present
+        expect(res["token-type"]).to be_present
       end
     end
 
     context "name が存在しないとき" do
       let(:params) { attributes_for(:user, name: nil) }
-      fit "エラーする" do
+      it "エラーする" do
         expect { subject }.to change { User.count }.by(0)
         res = JSON.parse(response.body)
         expect(response).to have_http_status(:unprocessable_entity)
