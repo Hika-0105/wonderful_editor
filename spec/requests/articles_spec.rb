c2f8349918cf76e61ae132e22532903f476ffa5e
佐藤　光 <hikahu0105@gmail.com>
2022-01-11 17:53:13 +0900

    公開記事、下書き選択機能追加に伴うテストの修正

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    let!(:article1) { create(:article, updated_at: 1.days.ago) }
-    let!(:article2) { create(:article, updated_at: 2.days.ago) }
-    let!(:article3) { create(:article) }
-    it "記事の一覧が取得できる" do
+    let!(:article1) { create(:article, :published, updated_at: 1.days.ago) }
+    let!(:article2) { create(:article, :published, updated_at: 2.days.ago) }
+    let!(:article3) { create(:article, :published) }
+    before { create(:article, :draft) }
+
+    it "公開記事の一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
       expect(response).to have_http_status(:ok)
@@ -21,11 +23,11 @@ RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /articles/:id" do
     subject { get(api_v1_article_path(article_id)) }
 
-    context "指定した記事が存在する時" do
-      let(:article) { create(:article) }
+    context "指定した記事が公開記事の時" do
+      let(:article) { create(:article, :published) }
       let(:article_id) { article.id }
 
-      it "指定した記事の値を取得できる" do # rubocop:disable RSpec/ExampleLength
+      it "指定した記事の値を取得できる" do  # rubocop:disable RSpec/ExampleLength
         subject
         res = JSON.parse(response.body)
         expect(response).to have_http_status(:ok)
@@ -38,11 +40,20 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
 
+    context "指定した記事が下書きの時" do
+      let(:article) { create(:article, :draft) }
+      let(:article_id) { article.id }
+
+      it "指定した記事の値を取得できない" do
+        expect(subject).to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
     context "指定した記事が存在しない時" do
       let(:article_id) { 10_000_000 }
 
       it "記事が存在しない" do
-        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
@@ -53,15 +64,33 @@ RSpec.describe "Api::V1::Articles", type: :request do
     let(:current_user) { create(:user) }
     let(:headers) { current_user.create_new_auth_token }
 
-    context "適切なパラメータが送信された時" do
-      let(:params) { { article: attributes_for(:article) } }
+    context "記事を公開状態で作成する時" do
+      let(:params) { { article: attributes_for(:article, :status_published) } }
 
-      it "レコードを作成する" do # rubocop:disable RSpec/ExampleLength
+      it "公開記事を作成する" do # rubocop:disable RSpec/ExampleLength
         expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
         res = JSON.parse(response.body)
         expect(res["title"]).to eq params[:article][:title]
         expect(res["body"]).to eq params[:article][:body]
         expect(response).to have_http_status(:ok)
+        expect(res["status"]).to eq "published"
+        header = response.header
+        expect(header["access-token"]).to be_present
+        expect(header["client"]).to be_present
+        expect(header["uid"]).to be_present
+      end
+    end
+
+    context "記事を下書きで作成する時" do
+      let(:params) { { article: attributes_for(:article, :status_draft) } }
+
+      it "下書きを作成する" do # rubocop:disable RSpec/ExampleLength
+        expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
+        res = JSON.parse(response.body)
+        expect(res["title"]).to eq params[:article][:title]
+        expect(res["body"]).to eq params[:article][:body]
+        expect(response).to have_http_status(:ok)
+        expect(res["status"]).to eq "draft"
         header = response.header
         expect(header["access-token"]).to be_present
         expect(header["client"]).to be_present
@@ -81,18 +110,32 @@ RSpec.describe "Api::V1::Articles", type: :request do
     subject { patch(api_v1_article_path(article_id), params: params, headers: headers) }
 
     let(:current_user) { create(:user) }
-    let(:article_id) { article.id }
-    let(:article) { create(:article, user: current_user) }
-    let(:params) { { article: { title: Faker::Lorem.word, created_at: 1.day.ago } } }
+    let(:params) { { article: attributes_for(:article, :status_published) } }
     let(:headers) { current_user.create_new_auth_token }
 
-    it "ログインユーザーの記事の修正を行う" do
-      expect { subject }.to change { article.reload.title }.from(article.title).to(params[:article][:title]) &
-                            not_change { article.reload.created_at }
-      header = response.header
-      expect(header["access-token"]).to be_present
-      expect(header["client"]).to be_present
-      expect(header["uid"]).to be_present
+    context "ログインしているユーザーの記事を修正するとき" do
+      let!(:article) { create(:article, :draft, user: current_user) }
+      let!(:article_id) { article.id }
+
+      it "ログインユーザーの記事の修正を行う" do
+        expect { subject }.to change { article.reload.title }.from(article.title).to(params[:article][:title]) &
+                              not_change { article.reload.created_at }
+        header = response.header
+        expect(header["access-token"]).to be_present
+        expect(header["client"]).to be_present
+        expect(header["uid"]).to be_present
+      end
+    end
+
+    context "他のユーザーの記事を更新しようとるすとき" do # rubocop:disable RSpec/MultipleMemoizedHelpers
+      let(:other_user) { create(:user) }
+      let!(:article) { create(:article, user: other_user) }
+      let!(:article_id) { article.id }
+
+      it "更新できない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+        change { Article.count }.by(0)
+      end
     end
   end
 
